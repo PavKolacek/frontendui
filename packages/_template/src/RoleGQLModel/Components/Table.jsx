728f91206d7a5be87894d55f71461dd43752c1ce
Hrbolek <profesor@hrbolek.cz>
2025-12-25 21:51:43 +0100

    better composition for read pages

--- packages/_template/src/RoleGQLModel/Components/Table.jsx
+++ packages/_template/src/RoleGQLModel/Components/Table.jsx
@@ -1,7 +1,81 @@
-import { Table as BaseTable } from "../../Base/Components/Table" 
+import { Table as BaseTable, KebabMenu } from "../../Base/Components/Table" 
+import { Link as BaseLink } from "../../Base/Components/Link" 
+import { Link, LinkURI } from "./Link"
+import { useNavigate } from "react-router"
+import { UpdateLink } from "../Mutations/Update"
+
+const CellName = ({ row, name }) => (
+    <td key={name}>
+        <Link item={row} />
+    </td>
+)
+
+const CellGroup = ({ row, name }) => (
+    <td key={name}>
+        <BaseLink item={row?.group || {}} />
+    </td>
+)
+
+const CellUser = ({ row, name }) => (
+    <td key={name}>
+        <BaseLink item={row?.user || {}} />
+    </td>
+)
+
+const CellRoleType = ({ row, name }) => (
+    <td key={name}>
+        <BaseLink item={row?.roletype || {}} />
+    </td>
+)
+
+
+const CellTools = ({ row, name }) => {
+    const navigate = useNavigate()
+    const {id, name: name_}  = row || {}
+    return (
+        <td>
+            <KebabMenu actions={[
+                { 
+                    // label: "Editovat", 
+                    label: "Editovat " + name_, 
+                    onClick: () => navigate(`${LinkURI.replace("view", "edit")}${id}`),
+                    children: (<UpdateLink item={row} className="form-control btn btn-outline-secondary">Upravit</UpdateLink>)
+                    // children: (<UpdateLink item={row} className="btn btn-outline-secondary">{name}</UpdateLink>)
+                },
+                { 
+                    // label: "Editovat", 
+                    label: "Smazat " + name_, 
+                    onClick: () => navigate(`${LinkURI.replace("view", "edit")}${id}`),
+                    children: (<UpdateLink item={row} className="form-control btn btn-outline-secondary">Odstranit</UpdateLink>)
+                    // children: (<UpdateLink item={row} className="btn btn-outline-secondary">{name}</UpdateLink>)
+                }
+            ]} />
+        </td>
+    )
+}
+
+const table_def = {
+    "roletype": {
+        label: "Typ role",
+        component: CellRoleType
+    },
+    "user": {
+        label: "Uživatel",
+        component: CellUser
+    },
+    "group": {
+        label: "Skupina",
+        component: CellGroup
+    },
+    
+    "tools": {
+        label: "Nástroje",
+        component: CellTools
+    }
+}
 
 export const Table = ({ data }) => {
     return (
-        <BaseTable data={data} />
+        <BaseTable data={data} table_def={table_def}/>
     )
 }
